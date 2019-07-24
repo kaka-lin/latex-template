c2f6e13990acf4b25272f9f8d4cf8d80bffb152d
kaka <vn503024@gmail.com>
2019-07-24 12:19:19 +0800

    Maintain: table of contents

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # pandoc version 1.xx
 # -N: 每個section加上編號
 # --toc: 加上目錄
-pandoc --latex-engine=xelatex --template=kaka-template --from markdown --listings --toc -N <file name>.md -o <file name>.pdf
+pandoc --latex-engine=xelatex --template=kaka-template-ubuntu --from markdown --listings --toc -N <file name>.md -o <file name>.pdf
 
 # pandoc version 2.xx
-#pandoc --pdf-engine=xelatex --template=kaka-template  <your file>.md -o <file name>.pdf
+#pandoc --pdf-engine=xelatex --template=kaka-template-<Mac or Ubuntu> --from markdown -Mlistings --toc -N <your file>.md -o <file name>.pdf
 
